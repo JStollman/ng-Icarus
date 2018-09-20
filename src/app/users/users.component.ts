72d62c36055643d29669af71f3ea20853503cd41
Jason Snider <root@jasonsnider.com>
2018-09-20 11:35:25 -0500

    Added the users service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsersService } from '../users.service';
+import { User } from '../user';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(): void{
+    this.usersService.getUsers().subscribe(
+      (response:any)=>{
+        this.users = response.users,
+        console.log(this.users)
+      }
+    );
   }
 
 }
